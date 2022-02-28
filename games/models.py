416b8ce9e832373a677d0f3a9797f6ed9efb6570
Sven <sven.hoyer@hotmail.com>
2022-02-28 21:39:59 +0100

    add games model

--- games/models.py
+++ games/models.py
@@ -1,3 +1,22 @@
-from django.db import models
+# Standard Library
+from operator import mod
 
-# Create your models here.
+# Third Party
+from django.db import models
+from django.utils import timezone
+
+# Library
+from account.models import Account
+from teams.models import TeamModel
+
+
+class GameModel(models.Model):
+
+    game_admin = models.ForeignKey(Account, on_delete=models.DO_NOTHING)
+    team = models.ForeignKey(TeamModel, on_delete=models.CASCADE)
+    datetime = models.DateTimeField(verbose_name="datetime")
+    created_at = models.DateTimeField(default=timezone.now)
+    home = models.BooleanField(verbose_name="HomeAdvantage", default=True)
+    winner = models.BooleanField(verbose_name="Winner", default=None)
+    points_team = models.IntegerField()
+    points_opponents = models.IntegerField()
