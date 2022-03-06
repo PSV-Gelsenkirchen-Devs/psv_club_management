04d50b88298f7ada47fd419136ea8c9aceaaeebd
Sven <sven.hoyer@hotmail.com>
2022-03-06 20:19:46 +0100

    add Bootstrap to scss, show User Profil as Modal with Bootstrap and htmx

--- apps/account/urls.py
+++ apps/account/urls.py
@@ -7,6 +7,10 @@ from . import views
 app_name = "account"
 urlpatterns = [
     path("", views.index, name="index"),
+<<<<<<< HEAD:apps/account/urls.py
     path("user_list", views.user_list_view, name="user_list_view"),
+=======
+    path("/user_list", views.user_list_view, name="user_list_view"),
+>>>>>>> ea137c7 (add Bootstrap to scss, show User Profil as Modal with Bootstrap and htmx):account/urls.py
     path("<int:user_id>", views.profil_view, name="profil_view"),
 ]
