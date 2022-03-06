c1daf915f8080836332aa27cc1ddac84821fe900
Sven <sven.hoyer@hotmail.com>
2022-03-06 20:20:20 +0100

    show teams as Table with htmx

--- teams/models.py
+++ teams/models.py
@@ -5,7 +5,20 @@ from django.db import models
 from account.models import Account
 
 
-class TeamModel(models.Model):
-    team_capitan = models.ForeignKey(Account, on_delete=models.DO_NOTHING)
-    name = models.CharField(verbose_name="Team Name", max_length=15)
-    player = models.ManyToManyField(Account)
+class Team(models.Model):
+    name = models.CharField(
+        verbose_name="Team Name", max_length=15, blank=True, null=True
+    )
+    league = models.CharField(verbose_name="Liga", max_length=15)
+    league_link = models.URLField(verbose_name="Turnier.de", max_length=200)
+    team_capitan = models.ForeignKey(
+        Account,
+        verbose_name="Mannschaftsführer",
+        on_delete=models.DO_NOTHING,
+        related_name="team_capitan",
+        blank=True,
+        null=True,
+    )
+    player = models.ManyToManyField(
+        Account, verbose_name="Spieler", related_name="players", blank=True
+    )
