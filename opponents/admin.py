44acd1fe3e25a2991b9be35fd7b10e385cfa73a3
Sven <sven.hoyer@hotmail.com>
2022-03-09 09:53:49 +0100

    change opponents model and add to admin

--- opponents/admin.py
+++ opponents/admin.py
@@ -1,3 +1,139 @@
+# Third Party
 from django.contrib import admin
+from django.contrib.admin import ModelAdmin
 
-# Register your models here.
+from .models import OppClub, OppPlayer, OppTeam
+
+
+class OppClubAdmin(ModelAdmin):
+    list_display = ("name",)
+    search_fields = ("name",)
+    readonly_fields = ()
+
+    filter_horizontal = ()
+    list_filter = ()
+    fieldsets = (
+        (
+            None,
+            {"fields": ("name",)},
+        ),
+        (
+            "Permissions",
+            {
+                "fields": (),
+            },
+        ),
+    )
+    add_fieldsets = (
+        (
+            None,
+            {
+                "classes": ("wide",),
+                "fields": ("name",),
+            },
+        ),
+    )
+
+    def get_player(self, obj):
+        return ", ".join([str(p) for p in obj.player.all()])
+
+
+class OppTeamAdmin(ModelAdmin):
+    list_display = ("name",)
+    search_fields = ("name",)
+    readonly_fields = ()
+
+    filter_horizontal = ()
+    list_filter = ()
+    fieldsets = (
+        (
+            None,
+            {"fields": ("name",)},
+        ),
+        (
+            "Permissions",
+            {
+                "fields": (),
+            },
+        ),
+    )
+    add_fieldsets = (
+        (
+            None,
+            {
+                "classes": ("wide",),
+                "fields": ("name",),
+            },
+        ),
+    )
+
+    def get_player(self, obj):
+        return ", ".join([str(p) for p in obj.player.all()])
+
+
+class OppPlayerAdmin(ModelAdmin):
+    list_display = ("club", "first_name", "last_name")
+    search_fields = ("club", "first_name", "last_name")
+    readonly_fields = ()
+
+    filter_horizontal = ()
+    list_filter = ()
+    fieldsets = (
+        (
+            None,
+            {"fields": ("club", "first_name", "last_name")},
+        ),
+        (
+            "Permissions",
+            {
+                "fields": (),
+            },
+        ),
+    )
+    add_fieldsets = (
+        (
+            None,
+            {
+                "classes": ("wide",),
+                "fields": ("club", "first_name", "last_name"),
+            },
+        ),
+    )
+
+
+class OppTeamAdmin(ModelAdmin):
+    list_display = ("name", "club", "get_player")
+    search_fields = ("name", "club", "player")
+    readonly_fields = ()
+
+    filter_horizontal = ("player",)
+    list_filter = ()
+    fieldsets = (
+        (
+            None,
+            {"fields": ("name", "club", "player")},
+        ),
+        (
+            "Permissions",
+            {
+                "fields": (),
+            },
+        ),
+    )
+    add_fieldsets = (
+        (
+            None,
+            {
+                "classes": ("wide",),
+                "fields": ("name", "club", "player"),
+            },
+        ),
+    )
+
+    def get_player(self, obj):
+        return ", ".join([str(p) for p in obj.player.all()])
+
+
+admin.site.register(OppClub, OppClubAdmin)
+admin.site.register(OppTeam, OppTeamAdmin)
+admin.site.register(OppPlayer, OppPlayerAdmin)
--- opponents/models.py
+++ opponents/models.py
@@ -3,16 +3,29 @@ from django.db import models
 
 
 class OppClub(models.Model):
-    name = models.CharField(verbose_name="OppClubName", max_length=30)
+    name = models.CharField(verbose_name="Verein", max_length=30)
+
+    def __str__(self):
+        return self.name
 
 
 class OppPlayer(models.Model):
-    club = models.ForeignKey(OppClub, on_delete=models.DO_NOTHING)
-    first_name = models.CharField(verbose_name="FirstName", max_length=30)
-    last_name = models.CharField(verbose_name="LastName", max_length=30)
+    club = models.ForeignKey(
+        OppClub, verbose_name="Verein", on_delete=models.DO_NOTHING
+    )
+    first_name = models.CharField(verbose_name="Vorname", max_length=30)
+    last_name = models.CharField(verbose_name="Nachname", max_length=30)
+
+    def __str__(self):
+        full_name = self.first_name + self.last_name
+        return full_name
 
 
 class OppTeam(models.Model):
     name = models.CharField(verbose_name="OppTeamName", max_length=15)
     club = models.ForeignKey(OppClub, on_delete=models.CASCADE)
-    player = models.ManyToManyField(OppPlayer)
+    player = models.ManyToManyField(OppPlayer, blank=True)
+
+    def __str__(self):
+        full_name = self.club.name + " - " + self.name
+        return full_name
