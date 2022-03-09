1007f183984535818f2a611dd1afd635df9c2fe9
Sven <sven.hoyer@hotmail.com>
2022-03-09 09:50:48 +0100

    fix / in user_list view url

--- apps/account/urls.py
+++ apps/account/urls.py
@@ -7,6 +7,6 @@ from . import views
 app_name = "account"
 urlpatterns = [
     path("", views.index, name="index"),
-    path("/user_list", views.user_list_view, name="user_list_view"),
+    path("user_list", views.user_list_view, name="user_list_view"),
     path("<int:user_id>", views.profil_view, name="profil_view"),
 ]
